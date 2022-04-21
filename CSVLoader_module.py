9016c6fb076b998554b993103be55a167847a722
isirmt <r119306@g.kobe-kosen.ac.jp>
2022-04-21 14:29:56 +0900

    format csv

--- CSVLoader_module.py
+++ CSVLoader_module.py
@@ -3,8 +3,11 @@ import csv
 
 def OpenCSV():
     csv_file = open("./data/kakeiho.csv", "r", encoding="ms932", errors="", newline="")
-
     return csv_file
 
 def CloseCSV(f):
     f.close()
+
+def FormatCSV(f):
+    csv_list = csv.reader(f, delimiter="", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+    return csv_list
